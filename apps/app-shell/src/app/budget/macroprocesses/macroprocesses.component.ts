fd514a1cf4e4a8ddc743a7b82bb926cea7adac62
Duvan Rodriguez <duvan.rodriguez@tecsersas.com>
2025-08-11 14:44:40 -0500

    Update macroprocesses component

--- apps/app-shell/src/app/budget/macroprocesses/macroprocesses.component.ts
+++ apps/app-shell/src/app/budget/macroprocesses/macroprocesses.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-macroprocesses',
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './macroprocesses.component.html',
   styleUrls: ['./macroprocesses.component.scss']
 })
